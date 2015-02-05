8b2de60913b1c39498491605e970dc637ddabf07
Giermann <sven.giermann@gmail.com>
2015-02-05 15:44:43 +0100

    Correct usage of .bind()
    Many thanks to samwize
    http://samwize.com/2013/09/01/how-you-can-pass-a-variable-into-callback-function-in-node-dot-js/

--- scripts/heizung.js
+++ scripts/heizung.js
@@ -27,9 +27,9 @@
 /*
 
 0 = firstId:	".Soll"		Soll-Temperatur
-1 = statusCnt:	".Count"	Zähler der Einschaltvorgänge (wenn externId angegeben, wird diese überwacht)
-2 = statusTime:	".Zeit"		Betriebsstundenzähler in Stunden
-3 = storage	".Vorrat"	Heizöl/Rohstoff-Vorrat, wenn "verbrauchStd" angegeben
+1 = countId:	".Count"	Zähler der Einschaltvorgänge (wenn externId angegeben, wird diese überwacht)
+2 = timeId:	".Zeit"		Betriebsstundenzähler in Stunden
+3 = storageId:	".Vorrat"	Heizöl/Rohstoff-Vorrat, wenn "usage" angegeben
 x = nextSoll			(Kessel kaum möglich)
 x = nextZeit			(Kessel kaum möglich)
 
@@ -47,7 +47,7 @@ var Kessel = {
     "istTempId":74306,		// vorhandener Datenpunkt mit Ist-Temperatur
     "relaisId":77100,		// vorhandener Datenpunkt für Relais der Brenneranforderung
     "statusId":77101,		// vorhandener Datenpunkt für Zählung der Brennerbetriebszeit/Anschaltzyklen (notfalls =relaisId)
-    "verbrauchStd":0.0205,	// Heizöl/Rohstoff-Verbrauch pro Betriebsstunde (hier in Hektoliter, dadurch bessere Anzeige)
+    "usage":0.0205,		// Heizöl/Rohstoff-Verbrauch pro Betriebsstunde (hier in Hektoliter, dadurch bessere Anzeige)
     "hysterese":10,
     "minTemp":40,
     "maxTemp":75
@@ -124,7 +124,10 @@ function logOutput(level, str) {
 //
 // Überwachung/Auslesen eines externen Zustandes (An-Überwachung mittels externem Programm)
 //
-function observeStatus(data, idCnt, idTime, usage, idRest) {
+// use:
+//   observeStatus.bind( Heizkreise[...] );
+//
+function observeStatus(data) {
     if (data.newState.value == data.oldState.value) return;
 
     logOutput(1, "[observeStatus] #" + data.id + " --> " + data.newState.value);
@@ -133,20 +136,20 @@ function observeStatus(data, idCnt, idTime, usage, idRest) {
     if ((data.oldState.value != null) && (data.oldState.value >= 0)) {
         if ((data.newState.value == true) || (data.newState.value > 0)) {
             // Einschaltvorgang
-            var cntOld = parseInt(getState(idCnt));
-            setState(idCnt, cntOld + 1);
-            logOutput(2, "[observeStatus] increase counter #" + idCnt + " --> " + cntOld + " + 1");
+            var cntOld = parseInt(getState(this.countId));
+            setState(this.countId, cntOld + 1);
+            logOutput(2, "[observeStatus] increase counter #" + this.countId + " --> " + cntOld + " + 1");
         } else if ((data.newState.value == false) || (data.newState.value == 0)) {
             // Ausschaltvorgang
-            var timeOld = parseFloat(getState(idTime));
+            var timeOld = parseFloat(getState(this.timeId));
             var duration = Date.parse(data.newState.timestamp) - Date.parse(data.oldState.lastchange);
-            setState(idTime, timeOld + (duration / 3600000));
-            logOutput(2, "[observeStatus] add time #" + idTime + " --> " + timeOld + " + " + duration + "ms");
+            setState(this.timeId, timeOld + (duration / 3600000));
+            logOutput(2, "[observeStatus] add time #" + this.timeId + " --> " + timeOld + " + " + duration + "ms");
 
-            if ((usage != null) && (idRest != null)) {
-                var storeOld = parseFloat(getState(idRest));
-                setState(idRest, storeOld - (usage * duration / 3600000));
-                logOutput(2, "[observeStatus] consumed " + (usage * duration / 3600000) + " of " + storeOld + " at #" + idRest);
+            if (this.usage && this.storageId) {
+                var storeOld = parseFloat(getState(this.storageId));
+                setState(this.storageId, storeOld - (this.usage * duration / 3600000));
+                logOutput(2, "[observeStatus] consumed " + (this.usage * duration / 3600000) + " of " + storeOld + " at #" + this.storageId);
             }
         }
     }
@@ -155,57 +158,60 @@ function observeStatus(data, idCnt, idTime, usage, idRest) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 //
-// Überwachung/Regulierung der Soll-Temperaturen
+// Überwachung/Regulierung der Soll-Temperaturen und Relaiszustände
 //
 // TODO: Bisher reine Speicherladung mittels Pumpe, noch KEIN Pumpenkreis, noch KEIN Mischerkreis
 //
-function observeTemp(data, kreis) {
-    var minTemp, maxTemp, istTemp, hysterese = 10;
+// use:
+//   observeTemp.bind( Heizkreise[...] );
+//   observeRelay.bind( Heizkreise[...] );
+//
+function observeTemp(data) {
+    var minTemp, maxTemp, istTemp;
 
-    if (kreis["hysterese"]) hysterese = kreis["hysterese"];
-    if (kreis["istTempId"]) {
-        istTemp = getState(kreis["istTempId"]);
-        minTemp = getState(kreis["firstId"]) - hysterese/2;
-        maxTemp = minTemp + hysterese;
-        if (kreis["minTemp"] && (kreis["minTemp"] > minTemp)) minTemp = kreis["minTemp"];
-        if (kreis["maxTemp"] && (kreis["maxTemp"] < maxTemp)) maxTemp = kreis["maxTemp"];
+    if (this.istTempId) {
+        istTemp = getState(this.istTempId);
+        minTemp = getState(this.firstId) - (this.hysterese ? this.hysterese/2 : 5);
+        maxTemp = minTemp + (this.hysterese ? this.hysterese : 10);
+        if (this.minTemp && (this.minTemp > minTemp)) minTemp = this.minTemp;
+        if (this.maxTemp && (this.maxTemp < maxTemp)) maxTemp = this.maxTemp;
     }
 
     if (!istTemp) {
-        logOutput(4, "[observeTemp] cannot read temperature for circuit #" + kreis["firstId"] + " on change of #" + data.id);
-    } else if (kreis["relaisId"]) {
-        var istRelais = getState(kreis["relaisId"]);
+        logOutput(4, "[observeTemp] cannot read temperature for circuit #" + this.firstId + " on change of #" + data.id);
+    } else if (this.relaisId) {
+        var istRelais = getState(this.relaisId);
         if ((istRelais != true) && (istTemp < minTemp)) {
-            logOutput(2, "[observeTemp] set relais #" + kreis["relaisId"] + " ON (temp " + istTemp + " < " + minTemp + ")");
-            setState(kreis["relaisId"], true);
+            logOutput(2, "[observeTemp] set relais #" + this.relaisId + " ON (temp " + istTemp + " < " + minTemp + ")");
+            setState(this.relaisId, true);
         } else if ((istRelais != false) && (istTemp > maxTemp)) {
-            logOutput(2, "[observeTemp] set relais #" + kreis["relaisId"] + " OFF (temp " + istTemp + " > " + maxTemp + ")");
-            setState(kreis["relaisId"], false);
+            logOutput(2, "[observeTemp] set relais #" + this.relaisId + " OFF (temp " + istTemp + " > " + maxTemp + ")");
+            setState(this.relaisId, false);
         } else {
-            logOutput(1, "[observeTemp] leave relais #" + kreis["relaisId"] + (istRelais ? " ON" : " OFF") + " (temp " + istTemp + " is " + minTemp + ".." + maxTemp + ")");
+            logOutput(1, "[observeTemp] leave relais #" + this.relaisId + (istRelais ? " ON" : " OFF") + " (temp " + istTemp + " is " + minTemp + ".." + maxTemp + ")");
         }
     }
 }
 
-function observeRelay(data, kreis) {
-    var sollTemp = getState(kreis["firstId"]);
+function observeRelay(data) {
+    var sollTemp = getState(this.firstId);
 
     // beim Einschalten des Relais, prüfen ob Kessel-Soll erhöht werden muss
     if (data.newState.value && (sollTemp > 0)) {
-        kreis["kesselAnf"] = sollTemp + (kreis["kesselPlus"] ? kreis["kesselPlus"] : 10);
-        if (kreis["kesselAnf"] > getState(Kessel["firstId"]))
-            setState(Kessel["firstId"], kreis["kesselAnf"]);
-        logOutput(2, "[observeRelay] set demand for ciruit #" + kreis["firstId"] + " to " + sollTemp + "°C");
+        this.kesselAnf = sollTemp + (this.kesselPlus ? this.kesselPlus : 10);
+        if (this.kesselAnf > getState(Kessel.firstId))
+            setState(Kessel.firstId, this.kesselAnf);
+        logOutput(2, "[observeRelay] set demand for ciruit #" + this.firstId + " to " + sollTemp + "°C");
     } else {
-        kreis["kesselAnf"] = 0;
+        this.kesselAnf = 0;
 
         // frage Maximum aller anderen Heizkreise ab, starte mit Kessel-Mindesttemperatur
-        sollTemp = (Kessel["minTemp"] ? Kessel["minTemp"] : 40);
+        sollTemp = (Kessel.minTemp ? Kessel.minTemp : 40);
         for (var HK in Heizkreise) {
-            if (sollTemp < Heizkreise[HK]["kesselAnf"]) sollTemp = Heizkreise[HK]["kesselAnf"];
+            if (sollTemp < Heizkreise[HK].kesselAnf) sollTemp = Heizkreise[HK].kesselAnf;
         }
-        setState(Kessel["firstId"], sollTemp);
-        logOutput(2, "[observeRelay] unset demand for ciruit #" + kreis["firstId"] + ", reset to " + sollTemp + "°C");
+        setState(Kessel.firstId, sollTemp);
+        logOutput(2, "[observeRelay] unset demand for ciruit #" + this.firstId + ", reset to " + sollTemp + "°C");
     }
 }
 
@@ -219,14 +225,14 @@ function getNominalTemp(kreis, zeit, wochentag) {
     var zsoll = "0000";
     var programm, progGestern;
 
-    if (kreis["tagesprogramm"]) {
+    if (kreis.tagesprogramm) {
         logOutput(2, "[getNominalTemp] read program for day " + wochentag);
-        if (kreis["tagesprogramm"][wochentag])
-            programm = kreis[kreis["tagesprogramm"][wochentag]];
+        if (kreis.tagesprogramm[wochentag])
+            programm = kreis[kreis.tagesprogramm[wochentag]];
     }
     if (!programm) {
         // wenn keine Sektion "tagesprogramm" gefunden, dann für alle Tage "programm" nutzen
-        programm = kreis["programm"];
+        programm = kreis.programm;
         if (programm)
             logOutput(2, "[getNominalTemp] read global program");
         else
@@ -259,13 +265,13 @@ function setNominalTemp() {
     for (var HK in Heizkreise) {
         // Soll-Temperatur nach Zeitplan nur bestimmen, wenn Datum/Uhrzeit stimmt; sonst Notlauf-Temperatur belassen
         if (now.getFullYear() < 2000) {
-            sollTemp = Heizkreise[HK]["notlauf"];
+            sollTemp = Heizkreise[HK].notlauf;
         } else {
             sollTemp = getNominalTemp(Heizkreise[HK], timeNow, heute);
 
             // wenn keine gefunden: zuletzt gesetzte Soll-Temperatur auslesen
             if (sollTemp < 0) {
-                sollTemp = getState(Heizkreise[HK]["firstId"]);
+                sollTemp = getState(Heizkreise[HK].firstId);
                 // ist diese unbrauchbar, letzte Soll-Temperatur des Vortages ermitteln
                 // HINWEIS: hier erfolgt keine Prüfung, ob gestern ein Feiertag war - sollte aber recht selten eintreten
                 if (!sollTemp) sollTemp = getNominalTemp(Heizkreise[HK], "2400", (now.getDay() + 6) % 7);
@@ -273,7 +279,7 @@ function setNominalTemp() {
         }
         if (sollTemp >= 0) {
             logOutput(2, "[setNominalTemp] set temp [" + timeNow + "] to " + sollTemp + "°C");
-            setState(Heizkreise[HK]["firstId"], sollTemp);
+            setState(Heizkreise[HK].firstId, sollTemp);
         }
     }
 
@@ -290,83 +296,69 @@ function setNominalTemp() {
 // Initialisierung(en)
 //
 function initDatapoints(name, kreis) {
-    setObject(kreis["firstId"], {
+    setObject(kreis.firstId, {
         Name: "Heizung." + name + ".Soll",
         TypeName: "VARDP",
         ValueUnit: "°C",
         _persistent: true
     });
-    logOutput(3, "[initDatapoints] created datapoint #" + kreis["firstId"] + " 'Heizung." + name + ".Soll'");
+    logOutput(3, "[initDatapoints] created datapoint #" + kreis.firstId + " 'Heizung." + name + ".Soll'");
 
     // Betriebsstundenzähler
-    if (kreis["statusId"]) {
-        kreis["statusCnt"] = kreis["firstId"] + 1;
-        setObject(kreis["statusCnt"], {
+    if (kreis.statusId) {
+        kreis.countId = kreis.firstId + 1;
+        setObject(kreis.countId, {
             Name: "Heizung." + name + ".Cnt",
             TypeName: "VARDP",
             _persistent: true
         });
-        logOutput(3, "[initDatapoints] created datapoint #" + kreis["statusCnt"] + " 'Heizung." + name + ".Cnt'");
+        logOutput(3, "[initDatapoints] created datapoint #" + kreis.countId + " 'Heizung." + name + ".Cnt'");
 
-        kreis["statusTime"] = kreis["firstId"] + 2;
-        setObject(kreis["statusTime"], {
+        kreis.timeId = kreis.firstId + 2;
+        setObject(kreis.timeId, {
             Name: "Heizung." + name + ".Zeit",
             TypeName: "VARDP",
             _persistent: true
         });
-        logOutput(3, "[initDatapoints] created datapoint #" + kreis["statusTime"] + " 'Heizung." + name + ".Zeit'");
+        logOutput(3, "[initDatapoints] created datapoint #" + kreis.timeId + " 'Heizung." + name + ".Zeit'");
 
-        if (kreis["verbrauchStd"]) {
-            kreis["storage"] = kreis["firstId"] + 3;
-            setObject(kreis["storage"], {
+        if (kreis.usage) {
+            kreis.storageId = kreis.firstId + 3;
+            setObject(kreis.storageId, {
                 Name: "Heizung." + name + ".Vorrat",
                 TypeName: "VARDP",
                 _persistent: true
             });
-            logOutput(3, "[initDatapoints] created datapoint #" + kreis["storage"] + " 'Heizung." + name + ".Vorrat'");
+            logOutput(3, "[initDatapoints] created datapoint #" + kreis.storageId + " 'Heizung." + name + ".Vorrat'");
         } else {
-            kreis["storage"] = null;
+            kreis.storageId = null;
         }
 
         subscribe({
-            id: kreis["statusId"]
-        }, function(data) {
-            observeStatus(data, kreis["statusCnt"], kreis["statusTime"], kreis["verbrauchStd"], kreis["storage"]);
-        });
+            id: kreis.statusId
+        },
+            observeStatus.bind( kreis )
+        );
     }
 
-    if (kreis["istTempId"]) {
+    if (kreis.istTempId) {
         // wenn Ist-Temperatur vorhanden, dann Soll- und Ist-Temperatur überwachen
         subscribe({
-            id: kreis["firstId"]
-        }, function(data) {
-            observeTemp(data, kreis);
-        });
+            id: kreis.firstId
+        },
+            observeTemp.bind( kreis )
+        );
 
         subscribe({
-            id: kreis["istTempId"]
-        }, function(data) {
-            observeTemp(data, kreis);
-        });
+            id: kreis.istTempId
+        },
+            observeTemp.bind( kreis )
+        );
     }
-
-    // Relaiszustand überwachen
-    if (kreis["relaisId"] && (kreis != Kessel)) {
-        subscribe({
-            id: kreis["relaisId"]
-        }, function(data) {
-            observeRelay(data, kreis);
-        });
-    }
-//    if (kreis["relaisId"]) {
-        // Anfangszustand auf null (undefiniert) setzen
-        // nur dadurch wird nach einem Neustart des Systems auch der Relaiszustand wieder gesetzt!
-//        setState(kreis["relaisId"], null);
-//    }
 }
 
 function initHeizung() {
-    if (!Kessel["firstId"]) {
+    if (!Kessel.firstId) {
         logOutput(5, "[initHeizung] Configuration error - exiting.");
         return;
     }
@@ -376,9 +368,17 @@ function initHeizung() {
     // Datenpunkte der Heizkreise setzen
     var cnt = 1;
     for (var HK in Heizkreise) {
-        Heizkreise[HK]["firstId"] = Kessel["firstId"] + (10 * cnt++);
+        Heizkreise[HK].firstId = Kessel.firstId + (10 * cnt++);
         initDatapoints(HK, Heizkreise[HK]);
 
+        // Relaiszustand überwachen
+        if (Heizkreise[HK].relaisId) {
+            subscribe({
+                id: Heizkreise[HK].relaisId
+            },
+                observeRelay.bind( Heizkreise[HK] )
+            );
+        }
         // TODO: nextSoll / nextZeit nur bei Heizkreisen
     }
 
